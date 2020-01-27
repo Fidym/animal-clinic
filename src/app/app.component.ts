85956e01c1243020856a6893904ba30710276d6f
marci <marci@fidy.hu>
2020-01-27 14:56:12 +0100

    securityFixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'clinic-app';
 
-  // firebaseConfig = {
-  //   apiKey: 'AIzaSyAR_ej4f6htTxh6YIcoMcwtwznuKyQnlzU',
-  //   authDomain: 'animal-clinic-b523e.firebaseapp.com',
-  //   databaseURL: 'https://animal-clinic-b523e.firebaseio.com',
-  //   projectId: 'animal-clinic-b523e',
-  //   storageBucket: 'animal-clinic-b523e.appspot.com',
-  //   messagingSenderId: '766265606711',
-  //   appId: '1:766265606711:web:812387e044859ae8'
-  // };
-
-  // constructor() {
-  //   firebase.initializeApp(this.firebaseConfig);
-  // }
-
 }
